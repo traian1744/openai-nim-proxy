295a0cfd716fe97c34346c56e6c5f407ebf6e538
traian1744 <81891474+traian1744@users.noreply.github.com>
2026-07-21 03:24:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-
+require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
